d364d37d7c6b348d5d903a2cd09dee85b8527e55
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-05 19:56:28 +0700

    test(models/entities) add basic tests for the CacheMeta record read/write operations as a json file
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/test/java/net/ygbstudio/powerwp4j/models/entities/CacheMetaTest.java
@@ -0,0 +1,59 @@
+package net.ygbstudio.powerwp4j.models.entities;
+
+import static net.ygbstudio.powerwp4j.utils.JsonSupport.readJsonFs;
+import static net.ygbstudio.powerwp4j.utils.JsonSupport.readValueFromJson;
+import static net.ygbstudio.powerwp4j.utils.JsonSupport.toJsonString;
+import static net.ygbstudio.powerwp4j.utils.JsonSupport.writeJsonFs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.io.File;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CacheMetaTest {
+
+  private CacheMeta sampleCacheMeta;
+
+  @BeforeEach
+  void setUp() {
+    sampleCacheMeta = new CacheMeta(10, 20, LocalDate.now());
+  }
+
+  @Test
+  void cacheMetaParseTest() {
+    String json = toJsonString(sampleCacheMeta);
+    CacheMeta parsedCacheMeta = readValueFromJson(json, CacheMeta.class);
+    assertEquals(sampleCacheMeta, parsedCacheMeta);
+  }
+
+  @Test
+  void cacheMetaEqualityTest() {
+    CacheMeta anotherCacheMeta = new CacheMeta(10, 20, sampleCacheMeta.lastUpdated());
+    assertEquals(sampleCacheMeta, anotherCacheMeta);
+  }
+
+  @Test
+  void cacheMetaInequalityTest() {
+    CacheMeta differentCacheMeta = new CacheMeta(15, 25, sampleCacheMeta.lastUpdated().plusDays(1));
+    assertNotEquals(sampleCacheMeta, differentCacheMeta);
+  }
+
+  @Test
+  void cacheMetaWriteTest() {
+    File sampleCacheMetaFile = new File("sample_cache_meta.json");
+
+    writeJsonFs(sampleCacheMetaFile, sampleCacheMeta);
+    if (!sampleCacheMetaFile.exists()) {
+
+      throw new AssertionError("Cache meta file was not created.");
+    }
+    CacheMeta writtenCacheMeta = readJsonFs(new File("sample_cache_meta.json"), CacheMeta.class);
+    assertEquals(sampleCacheMeta, writtenCacheMeta);
+    assertEquals(writtenCacheMeta.cachedPages(), sampleCacheMeta.cachedPages());
+    assertEquals(writtenCacheMeta.cachedPosts(), sampleCacheMeta.cachedPosts());
+    assertEquals(writtenCacheMeta.lastUpdated(), sampleCacheMeta.lastUpdated());
+    sampleCacheMetaFile.deleteOnExit();
+  }
+}
