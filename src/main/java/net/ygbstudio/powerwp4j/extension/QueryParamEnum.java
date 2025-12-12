b470b8980f2675ecd1349d7b3d25be043b2f2e07
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 16:13:58 +0700

    style(QueryParamEnum): reformat joinQueryParams Javadoc for improved readability
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/extension/QueryParamEnum.java
+++ src/main/java/net/ygbstudio/powerwp4j/extension/QueryParamEnum.java
@@ -36,28 +36,31 @@ import org.jspecify.annotations.NonNull;
  */
 public interface QueryParamEnum {
 
-    /**
-     * This method helps you join a map of query parameters into a string that can be used as a query string.
-     *
-     * <p>You need to have at least one "leading" (starting with '?') or main query parameter in your map.
-     * This is the parameter that will be used to separate the query parameters from the rest of the URL
-     * and avoid malformed requests.
-     *
-     * <p>It is up to you to create a separate class that contains such "leading"
-     * parameters or use the same for all as long as both implement {@link QueryParamEnum}.
-     * In either case this method will help you and throw an {@link IllegalArgumentException}
-     * if you do not provide a single query parameter that starts with '?' with its value pair.
-     *
-     * @param wpRestQueriesMap <p>Map of query parameters and their values.
-     * @return <p>String that can be used as a query string.
-     */
+  /**
+   * This method helps you join a map of query parameters into a string that can be used as a query
+   * string.
+   *
+   * <p>You need to have at least one "leading" (starting with '?') or main query parameter in your
+   * map. This is the parameter that will be used to separate the query parameters from the rest of
+   * the URL and avoid malformed requests.
+   *
+   * <p>It is up to you to create a separate class that contains such "leading" parameters or use
+   * the same for all as long as both implement {@link QueryParamEnum}. In either case this method
+   * will help you and throw an {@link IllegalArgumentException} if you do not provide a single
+   * query parameter that starts with '?' with its value pair.
+   *
+   * @param wpRestQueriesMap
+   *     <p>Map of query parameters and their values.
+   * @return
+   *     <p>String that can be used as a query string.
+   */
   static <E extends QueryParamEnum> @NonNull String joinQueryParams(
       Map<E, String> wpRestQueriesMap) {
     if (wpRestQueriesMap.isEmpty()) return "";
     if (wpRestQueriesMap.keySet().stream().filter(param -> param.toString().startsWith("?")).count()
         != 1) {
       throw new IllegalArgumentException(
-              "You need to include at least, and only one, leading query parameter or a query string start key.");
+          "You need to include at least, and only one, leading query parameter or a query string start key.");
     }
     Comparator<E> leadingParam = Comparator.comparing(param -> param.toString().startsWith("?"));
     List<E> sortedParams = wpRestQueriesMap.keySet().stream().sorted(leadingParam).toList();
