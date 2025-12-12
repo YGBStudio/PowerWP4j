821c61b01ab1a2da121b4fae35ac2de9c4760482
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 13:03:17 +0700

    feat(models.schema): add WPRestPath enum for WordPress REST API endpoints
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/models/schema/WPRestPath.java
@@ -0,0 +1,64 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.models.schema;
+
+import java.time.YearMonth;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * Enum representing various API endpoints and query parameters for WordPress REST API.
+ *
+ * <p>This enum includes common endpoints and parameters used to interact with WordPress data, such
+ * as users, posts, media, and categories. Each enum constant is associated with its corresponding
+ * string representation used in the API URLs.
+ *
+ * <p><strong>Assume the endpoint is provided with a leading slash and the implementation will add
+ * any trailing slashes if needed.
+ *
+ * @author Yoham Gabriel @ YGB Studio
+ */
+@NullMarked
+public enum WPRestPath {
+  USERS("/users"),
+  POSTS("/posts"),
+  PHOTOS("/photos"),
+  CATEGORIES("/categories"),
+  MEDIA("/media"),
+  TAGS("/tags"),
+  CONTENT_UPLOADS(
+      "/wp-content/uploads/"
+          + YearMonth.now().getYear()
+          + "/"
+          + String.format("%02d", YearMonth.now().getMonthValue()));
+
+  private final String value;
+
+  WPRestPath(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return this.value;
+  }
+}
