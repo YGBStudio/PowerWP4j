900a95cabb059befcad2baf2de4264424a15d53a
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 16:06:43 +0700

    feat(QueryParamEnum): introduce unified interface for custom query parameters with joinQueryParams method
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/extension/QueryParamEnum.java
@@ -0,0 +1,88 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.extension;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * QueryParamEnum is an interface that promotes a unified type for query parameters and a {@code
+ * toString} method to ensure consistency with the design practices of the project. It can be used
+ * as a means to provide your own query parameters to the WPSiteEngine safely.
+ *
+ * @author Yoham Gabriel B. @YGBStudio
+ */
+public interface QueryParamEnum {
+
+    /**
+     * This method helps you join a map of query parameters into a string that can be used as a query string.
+     *
+     * <p>You need to have at least one "leading" (starting with '?') or main query parameter in your map.
+     * This is the parameter that will be used to separate the query parameters from the rest of the URL
+     * and avoid malformed requests.
+     *
+     * <p>It is up to you to create a separate class that contains such "leading"
+     * parameters or use the same for all as long as both implement {@link QueryParamEnum}.
+     * In either case this method will help you and throw an {@link IllegalArgumentException}
+     * if you do not provide a single query parameter that starts with '?' with its value pair.
+     *
+     * @param wpRestQueriesMap <p>Map of query parameters and their values.
+     * @return <p>String that can be used as a query string.
+     */
+  static <E extends QueryParamEnum> @NonNull String joinQueryParams(
+      Map<E, String> wpRestQueriesMap) {
+    if (wpRestQueriesMap.isEmpty()) return "";
+    if (wpRestQueriesMap.keySet().stream().filter(param -> param.toString().startsWith("?")).count()
+        != 1) {
+      throw new IllegalArgumentException(
+              "You need to include at least, and only one, leading query parameter or a query string start key.");
+    }
+    Comparator<E> leadingParam = Comparator.comparing(param -> param.toString().startsWith("?"));
+    List<E> sortedParams = wpRestQueriesMap.keySet().stream().sorted(leadingParam).toList();
+
+    StringBuilder pathString = new StringBuilder();
+    sortedParams
+        .reversed()
+        .forEach(
+            param -> {
+              if (!pathString.isEmpty() && !param.toString().startsWith("?"))
+                pathString.append("&");
+              pathString.append(param);
+              pathString.append(wpRestQueriesMap.get(param));
+            });
+
+    return pathString.toString();
+  }
+
+  /**
+   * Returns the string representation of the query parameter. This is a common override of the
+   * {@link Object#toString()} method. The design recommendation of this project is to return the
+   * {@code Enum} value as the string representation of the query parameter to make sure the methods
+   * and helper utilities work as expected in this project.
+   *
+   * @return the string value of the query parameter as provided in its constructor
+   */
+  String toString();
+}
