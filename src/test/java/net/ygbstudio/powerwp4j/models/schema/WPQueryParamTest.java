9f571fe14b998781ffd0997a8a8eb1c5cfa73f45
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 13:06:58 +0700

    test(models.schema): add WPQueryParam joinQueryParams tests
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/test/java/net/ygbstudio/powerwp4j/models/schema/WPQueryParamTest.java
@@ -0,0 +1,53 @@
+package net.ygbstudio.powerwp4j.models.schema;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+import org.hamcrest.core.StringRegularExpression;
+import org.junit.jupiter.api.Test;
+
+class WPQueryParamTest {
+
+  private final Map<WPQueryParam, String> queryParamMap =
+      Map.of(
+          WPQueryParam.PAGE, "3",
+          WPQueryParam.PER_PAGE, "8");
+
+  private final Pattern expectedRegEx =
+      Pattern.compile("\\?[a-zA-Z0-9_-]+=[a-zA-Z0-9_-]+(?:&[a-zA-Z0-9_-]+=[a-zA-Z0-9_-]+)*$");
+
+  @Test
+  void joinQueryParamsTest() {
+    String resultingPath = WPQueryParam.joinQueryParams(queryParamMap);
+    assertThat(resultingPath, StringRegularExpression.matchesRegex(expectedRegEx));
+  }
+
+  @Test
+  void joinQueryParamsMapReversed() {
+    LinkedHashMap<WPQueryParam, String> reverseMap = new LinkedHashMap<>(queryParamMap);
+    reverseMap.reversed();
+    String resultingPathReversedParams = WPQueryParam.joinQueryParams(reverseMap);
+    assertThat(resultingPathReversedParams, StringRegularExpression.matchesRegex(expectedRegEx));
+  }
+
+  @Test
+  void joinQueryParamsWithTimestamp() {
+    Map<WPQueryParam, String> queryParams =
+        Map.of(
+            WPQueryParam.PAGE, "3",
+            WPQueryParam.PER_PAGE, "8",
+            WPQueryParam.TIMESTAMP, String.valueOf(System.currentTimeMillis()));
+    String resultingPath = WPQueryParam.joinQueryParams(queryParams);
+    assertThat(resultingPath, StringRegularExpression.matchesRegex(expectedRegEx));
+  }
+
+  @Test
+  void joinQueryParamEmptyMapTest() {
+    String resultingPath = WPQueryParam.joinQueryParams(Collections.emptyMap());
+    assertThat(resultingPath, is(""));
+  }
+}
