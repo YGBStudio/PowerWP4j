a319411fb695dc4c22bc85cfcbba5e47bd9c05dd
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 11:31:02 +0700

    refactor(WPSiteEngine): add static import for jsonReader
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/engine/WPSiteEngine.java
+++ src/main/java/net/ygbstudio/powerwp4j/engine/WPSiteEngine.java
@@ -23,6 +23,7 @@
 package net.ygbstudio.powerwp4j.engine;
 
 import static net.ygbstudio.powerwp4j.services.ApiService.makeRequestURL;
+import static net.ygbstudio.powerwp4j.utils.JsonSupport.jsonReader;
 import static net.ygbstudio.powerwp4j.utils.JsonSupport.readJsonFs;
 import static net.ygbstudio.powerwp4j.utils.JsonSupport.writeJsonFs;
 
