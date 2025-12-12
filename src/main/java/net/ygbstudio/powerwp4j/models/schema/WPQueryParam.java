ebb25c9467ef3c84ad278f79ae47d5b78920ac5d
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 16:11:44 +0700

    refactor(WPQueryParam): remove custom joinQueryParams method in favor of QueryParamEnum implementation
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/models/schema/WPQueryParam.java
+++ src/main/java/net/ygbstudio/powerwp4j/models/schema/WPQueryParam.java
@@ -42,31 +42,6 @@ public enum WPQueryParam implements QueryParamEnum {
     this.value = value;
   }
 
-  public static String joinQueryParams(Map<WPQueryParam, String> wpRestQueriesMap) {
-    if (wpRestQueriesMap.isEmpty()) return "";
-    if (wpRestQueriesMap.keySet().stream().filter(param -> param.toString().startsWith("?")).count()
-        != 1) {
-      throw new IllegalArgumentException("You need to include only one leading query parameter");
-    }
-    Comparator<WPQueryParam> leadingParam =
-        Comparator.comparing(param -> param.toString().startsWith("?"));
-    List<WPQueryParam> sortedParams =
-        wpRestQueriesMap.keySet().stream().sorted(leadingParam).toList();
-
-    StringBuilder pathString = new StringBuilder();
-    sortedParams
-        .reversed()
-        .forEach(
-            param -> {
-              if (!pathString.isEmpty() && !param.toString().startsWith("?"))
-                pathString.append("&");
-              pathString.append(param);
-              pathString.append(wpRestQueriesMap.get(param));
-            });
-
-    return pathString.toString();
-  }
-
   @Override
   public String toString() {
     return value;
