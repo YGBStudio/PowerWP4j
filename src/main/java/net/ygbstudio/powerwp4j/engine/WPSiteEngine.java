e5528bce52b3caf562d5428737d06ee2b3c3a480
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-12 16:32:12 +0700

    docs(WPSiteEngine): add JavaDoc for updateCacheMeta method
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/engine/WPSiteEngine.java
+++ src/main/java/net/ygbstudio/powerwp4j/engine/WPSiteEngine.java
@@ -169,6 +169,13 @@ public class WPSiteEngine {
     return ApiService.connectGetWP(url, username, applicationPassword, wpSiteEngineLogger);
   }
 
+  /**
+   * Updates the cache metadata fields based on the WordPress response headers
+   * {@code x-wp-total} and {@code x-wp-totalpages}.
+   *
+   * @throws IOException if an I/O error occurs
+   * @throws InterruptedException if the thread is interrupted
+   */
   private void updateCacheMeta() throws IOException, InterruptedException {
     Optional<HttpResponse<String>> headersRequest =
         connectWP(
