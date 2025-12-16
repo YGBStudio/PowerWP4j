6916e0c85e0bcff47a9716a388748955029b1f99
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-16 18:38:13 +0700

    feat(schema): add WPFormat enum implementing PostFormatEnum
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/models/schema/WPFormat.java
@@ -0,0 +1,53 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.models.schema;
+
+import net.ygbstudio.powerwp4j.base.extension.PostFormatEnum;
+
+/**
+ * Enum class that represents formats in WordPress.
+ *
+ * @author Yoham Gabriel @ YGBStudio
+ */
+public enum WPFormat implements PostFormatEnum {
+  STANDARD("standard"),
+  ASIDE("aside"),
+  LINK("link"),
+  STATUS("status"),
+  QUOTE("quote"),
+  GALLERY("gallery"),
+  CHAT("chat"),
+  VIDEO("video"),
+  AUDIO("audio");
+
+  private final String value;
+
+  WPFormat(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
