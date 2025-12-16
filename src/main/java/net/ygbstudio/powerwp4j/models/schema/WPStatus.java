6dda16c0278c1752d211d6d488148c09a17abeb7
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-16 18:40:11 +0700

    feat(schema): add WPStatus enum implementing PostStatusEnum
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/models/schema/WPStatus.java
@@ -0,0 +1,49 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.models.schema;
+
+import net.ygbstudio.powerwp4j.base.extension.PostStatusEnum;
+
+/**
+ * Enum class that represents statuses in WordPress.
+ *
+ * @author Yoham Gabriel @ YGBStudio
+ */
+public enum WPStatus implements PostStatusEnum {
+  PUBLISH("publish"),
+  PRIVATE("private"),
+  DRAFT("draft"),
+  PENDING("pending"),
+  TRASH("trash");
+
+  private final String value;
+
+  WPStatus(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
