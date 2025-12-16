acd03001354ae1f3e7ba1b7b5ef00017d6b45786
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-16 18:28:50 +0700

    feat(CommentStatusEnum): add base extension interface for comment status enums
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/base/extension/CommentStatusEnum.java
@@ -0,0 +1,34 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.base.extension;
+
+import net.ygbstudio.powerwp4j.base.FriendlyEnum;
+
+/**
+ * Interface for comment status enums that provide a unified type for addition of custom and
+ * built-in comment statuses.
+ *
+ * @see FriendlyEnum
+ * @author Yoham Gabriel @ YGB Studio
+ */
+public interface CommentStatusEnum extends FriendlyEnum {}
