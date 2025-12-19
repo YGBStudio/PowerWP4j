5933dfdc1ab6985468567de9149ac52840102220
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-19 16:05:17 +0700

    docs(WPCacheKeySnapshot): clarify intended usage and document nullable values in cache subkey snapshots
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/models/entities/WPCacheKeySnapshot.java
+++ src/main/java/net/ygbstudio/powerwp4j/models/entities/WPCacheKeySnapshot.java
@@ -25,9 +25,16 @@ package net.ygbstudio.powerwp4j.models.entities;
 import java.util.Map;
 import net.ygbstudio.powerwp4j.base.extension.CacheKeyEnum;
 import net.ygbstudio.powerwp4j.base.extension.CacheSubKeyEnum;
+import org.jspecify.annotations.Nullable;
 
 /**
- * WPCacheKeySnapshot is a record that represents a snapshot of a cache key and its subkeys.
+ * WPCacheKeySnapshot is a record that represents a snapshot of a cache key and its subkeys. Use
+ * this abstraction if you are getting nested values and a key exists. If the values are contained
+ * in an array or immediate value (not inside another JSON object), you can use
+ *
+ * <p>{@link net.ygbstudio.powerwp4j.engine.WPCacheAnalyzer#getCacheKeyValueStream(CacheKeyEnum)}
+ *
+ * <p>Consumers must be wary that values in the subkey map can be {@code null}.
  *
  * @param <V> the type of the values in the cache subkey map. It is useful to think of this
  *     parameter as the kind of transformation that you apply to the cache subkeys. For example,
@@ -37,4 +44,4 @@ import net.ygbstudio.powerwp4j.base.extension.CacheSubKeyEnum;
  *     and their corresponding values of type {@code V}.
  */
 public record WPCacheKeySnapshot<V>(
-    CacheKeyEnum cacheKey, Map<CacheSubKeyEnum, V> cacheSubKeyMap) {}
+    CacheKeyEnum cacheKey, Map<CacheSubKeyEnum, @Nullable V> cacheSubKeyMap) {}
