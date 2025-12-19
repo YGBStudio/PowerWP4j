42433b141d9e1a0d035535a678e8d52b29db9522
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2025-12-19 15:05:25 +0700

    feat(EnvironmentScope): centralize WordPress environment variable keys with a friendly enum representation
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

---
+++ src/main/java/net/ygbstudio/powerwp4j/base/EnvironmentScope.java
@@ -0,0 +1,48 @@
+/*
+ * PowerWP4j - Power WP for Java
+ * Copyright (C) 2025 Yoham Gabriel Barboza B.
+ *
+ * This file is part of PowerWP4j.
+ *
+ * PowerWP4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerWP4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package net.ygbstudio.powerwp4j.base;
+
+/**
+ * Enumeration that represents the keys for the environment variables required to connect to a
+ * WordPress site.
+ *
+ * @implNote This enum implements the {@link FriendlyEnum} interface to enforce a user-friendly
+ *     string representation of the enum constants.
+ * @author Yoham Gabriel @ YGBStudio
+ */
+public enum EnvironmentScope implements FriendlyEnum {
+  WP_USER("wp.user"),
+  WP_APPLICATION_PASS("wp.appPass"),
+  WP_FULLY_QUALIFIED_DOMAIN_NAME("wp.fqdm");
+
+  private final String value;
+
+  EnvironmentScope(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value;
+  }
+}
