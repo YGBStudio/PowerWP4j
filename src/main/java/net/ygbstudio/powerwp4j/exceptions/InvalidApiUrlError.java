e3bdefddda6795e2e075d220ec908063a029b2e2
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-28 15:53:37 +0700

    docs(exceptions): clean up Javadoc formatting for InvalidApiUrlError
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/exceptions/InvalidApiUrlError.java
+++ src/main/java/net/ygbstudio/powerwp4j/exceptions/InvalidApiUrlError.java
@@ -23,8 +23,7 @@ package net.ygbstudio.powerwp4j.exceptions;
 import java.net.URISyntaxException;
 
 /**
- * Exception thrown when an invalid API URL is provided and any of the internal
- * methods of PowerWP4j
+ * Exception thrown when an invalid API URL is provided and any of the internal methods of PowerWP4j
  * detect a {@link URISyntaxException}.
  *
  * @author Yoham Gabriel @ YGB Studio
