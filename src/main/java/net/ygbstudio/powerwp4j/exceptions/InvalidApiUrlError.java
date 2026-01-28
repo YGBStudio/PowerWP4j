29dd2b1541811dc29bf0b71c4ad7b800b768264e
Yoham Gabriel B. @Urbine <yohamg@programmer.net>
2026-01-28 15:45:56 +0700

    refactor(exceptions): replace Error with RuntimeException for recoverable invalid api url failures
    Signed-off-by: Yoham Gabriel B. @Urbine <yohamg@programmer.net>

--- src/main/java/net/ygbstudio/powerwp4j/exceptions/InvalidApiUrlError.java
+++ src/main/java/net/ygbstudio/powerwp4j/exceptions/InvalidApiUrlError.java
@@ -23,12 +23,13 @@ package net.ygbstudio.powerwp4j.exceptions;
 import java.net.URISyntaxException;
 
 /**
- * Exception thrown when an invalid API URL is provided and any of the internal methods of PowerWP4j
+ * Exception thrown when an invalid API URL is provided and any of the internal
+ * methods of PowerWP4j
  * detect a {@link URISyntaxException}.
  *
  * @author Yoham Gabriel @ YGB Studio
  */
-public class InvalidApiUrlError extends Error {
+public class InvalidApiUrlError extends RuntimeException {
   public InvalidApiUrlError(String message) {
     super(message);
   }
